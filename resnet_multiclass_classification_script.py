ed508300224371ade73418dcc8d0bf0ddcf700f1
Noemie Ribaillier <noemie.ribaillier@gmail.com>
2025-03-14 20:22:41 +0100

    update the resnet multiclass classification script

--- resnet_multiclass_classification_script.py
+++ resnet_multiclass_classification_script.py
@@ -2,7 +2,7 @@
 #####                                                                                                    #####
 #####                          MULTICLASS CLASSIFICATION USING RESIDUALS NETWORK                         #####
 #####                                       Created on: 2025-02-21                                       #####
-#####                                       Updated on: 2025-02-26                                       #####
+#####                                       Updated on: 2025-03-14                                       #####
 #####                                                                                                    #####
 ##############################################################################################################
 
@@ -16,22 +16,18 @@ globals().clear()
 # Load the libraries
 import tensorflow as tf
 import numpy as np
-import scipy.misc
-from tensorflow.keras.applications.resnet_v2 import ResNet50V2, preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras import layers
-from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D, BatchNormalization
+from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, BatchNormalization
 from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.initializers import random_uniform, glorot_uniform, constant, identity
-from tensorflow.python.framework.ops import EagerTensor
+from tensorflow.keras.initializers import random_uniform, glorot_uniform
 from matplotlib.pyplot import imshow
 import matplotlib.pyplot as plt
 
-# We set up a seed to have reproducible results
+# Set up a seed to have reproducible results
 np.random.seed(1)
 tf.random.set_seed(2)
 
-# We set up the right directory
+# Set up the right directory
 import os
 os.chdir('C:/Users/Admin/Documents/Python Projects/image_classification')
 from general_functions import *
@@ -58,30 +54,28 @@ def identity_block(X, f, filters, initializer=random_uniform):
     Returns:
     X -- output of the identity block, tensor of shape (m, n_H, n_W, n_C)
     """
-    
-    # Retrieve Filters
+    # Retrieve filters
     F1, F2, F3 = filters
     
-    # Save the input value. You'll need this later to add back to the main path. 
+    # Save the input value. We'll need this later to add back to the main path. 
     X_shortcut = X
     
-    # First component of main path
+    # 1st component of main path: Conv2D -> BN -> ReLU
     X = Conv2D(filters = F1, kernel_size = 1, strides = (1,1), padding = 'valid', kernel_initializer = initializer(seed=0))(X)
-    # If training is set to False, its weights are not updated with the new examples. I.e when the model is used in prediction mode.
-    # axis: Integer, the axis that should be normalized (typically the features axis), here we are normalizing the channels axis
+    # axis: integer, the axis that should be normalized (typically the features axis), here we are normalizing the channels axis
     X = BatchNormalization(axis = 3)(X) 
     X = Activation('relu')(X)
     
-    ## Second component of main path
+    # 2nd component of main path: Conv2D -> BN -> ReLU
     X = Conv2D(filters = F2, kernel_size = f, strides = (1,1), padding = 'same', kernel_initializer = initializer(seed=0))(X)
     X = BatchNormalization(axis = 3)(X) 
     X = Activation('relu')(X)
 
-    ## Third component of main path
+    # 3rd component of main path: Conv2D -> BN
     X = Conv2D(filters = F3, kernel_size = 1, strides = (1,1), padding = 'valid', kernel_initializer = initializer(seed=0))(X)
     X = BatchNormalization(axis = 3)(X) 
     
-    ## Final step: Add shortcut value to main path, and pass it through a RELU activation
+    # Final step: add shortcut value to main path, and pass it through a ReLU activation
     X = Add()([X_shortcut,X])
     X = Activation('relu')(X)
 
@@ -111,31 +105,28 @@ def convolutional_block(X, f, filters, s = 2, initializer=glorot_uniform):
     Returns:
     X -- output of the convolutional block, tensor of shape (m, n_H, n_W, n_C)
     """
-    
-    # Retrieve Filters
+    # Retrieve filters
     F1, F2, F3 = filters
     
     # Save the input value
     X_shortcut = X
-
-    ##### MAIN PATH #####
     
-    # First component of main path
+    # 1st component of main path: Conv2D -> BN -> ReLU
     X = Conv2D(filters = F1, kernel_size = 1, strides = (s, s), padding='valid', kernel_initializer = initializer(seed=0))(X)
-    # axis: Integer, the axis that should be normalized (typically the features axis), here we are normalizing the channels axis
+    # axis: integer, the axis that should be normalized (typically the features axis), here we are normalizing the channels axis
     X = BatchNormalization(axis = 3)(X)
     X = Activation('relu')(X) # no hyperparameters
     
-    ## Second component of main path
+    # 2nd component of main path: Conv2D -> BN -> ReLU
     X = Conv2D(filters = F2, kernel_size = f, strides = (1,1), padding='same', kernel_initializer = initializer(seed=0))(X)
     X = BatchNormalization(axis = 3)(X)
     X = Activation('relu')(X)   
 
-    ## Third component of main path (no ReLU activation function in this component)
+    # 3rd component of main path: Conv2D -> BN
     X = Conv2D(filters = F3, kernel_size = 1, strides = (1,1), padding='valid', kernel_initializer = initializer(seed=0))(X)
     X = BatchNormalization(axis = 3)(X)
     
-    ##### SHORTCUT PATH #####
+    # Shortcut path
     X_shortcut = Conv2D(filters = F3, kernel_size = 1, strides = (s,s), padding='valid', kernel_initializer = initializer(seed=0))(X_shortcut)
     X_shortcut = BatchNormalization(axis = 3)(X_shortcut)
 
@@ -151,44 +142,43 @@ def convolutional_block(X, f, filters, s = 2, initializer=glorot_uniform):
 ##############################################################################################################
 
 # Create the function implementing the ResNet model with 50 layers
-def ResNet50(input_shape = (64, 64, 3), classes = 6, training=False):
+def ResNet50(input_shape = (64, 64, 3), nb_classes = 6, training=False):
     """
-    Stage-wise implementation of the architecture of the popular ResNet50:
+    Stage-wise implementation of the architecture of the ResNet50:
     CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
     -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> FLATTEN -> DENSE 
 
     Arguments:
     input_shape -- shape of the images of the dataset
-    classes -- integer, number of classes
+    nb_classes -- number of classes (integer)
 
     Returns:
     model -- a Model() instance in Keras
     """
-    
     # Define the input as a tensor with shape input_shape
     X_input = Input(input_shape)
 
     # Zero-Padding
     X = ZeroPadding2D((3, 3))(X_input)
     
-    # Stage 1
+    # Stage 1: Conv2D -> BN -> ReLU -> MaxPool
     X = Conv2D(64, (7, 7), strides = (2, 2), kernel_initializer = glorot_uniform(seed=0))(X)
     X = BatchNormalization(axis = 3)(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((3, 3), strides=(2, 2))(X)
     
-    # Stage 2
+    # Stage 2: CONVBLOCK -> IDBLOCK*2
     X = convolutional_block(X, f = 3, filters = [64, 64, 256], s = 1)
     X = identity_block(X, 3, [64, 64, 256])
     X = identity_block(X, 3, [64, 64, 256])
 
-    ## Stage 3
+    # Stage 3: CONVBLOCK -> IDBLOCK*3
     X = convolutional_block(X, f = 3, filters = [128,128,512], s = 2)
     X = identity_block(X, 3, [128,128,512])
     X = identity_block(X, 3, [128,128,512])
     X = identity_block(X, 3, [128,128,512])
 
-    # Stage 4 
+    # Stage 4: CONVBLOCK -> IDBLOCK*5
     X = convolutional_block(X, f = 3, filters = [256, 256, 1024], s = 2)
     X = identity_block(X, 3, [256, 256, 1024])
     X = identity_block(X, 3, [256, 256, 1024])
@@ -196,7 +186,7 @@ def ResNet50(input_shape = (64, 64, 3), classes = 6, training=False):
     X = identity_block(X, 3, [256, 256, 1024])
     X = identity_block(X, 3, [256, 256, 1024])
 
-    # Stage 5
+    # Stage 5: CONVBLOCK -> IDBLOCK*2
     X = convolutional_block(X, f = 3, filters = [512, 512, 2048], s = 2)
     X = identity_block(X, 3, [512, 512, 2048])
     X = identity_block(X, 3, [512, 512, 2048])
@@ -204,10 +194,10 @@ def ResNet50(input_shape = (64, 64, 3), classes = 6, training=False):
     # AVGPOOL
     X = AveragePooling2D(pool_size=(2, 2))(X)
 
-    # output layer
+    # Output layer: FLATTEN -> DENSE 
     X = Flatten()(X) # no hyperparameters
     # FC (= Dense) layer reduces its input to the number of classes
-    X = Dense(classes, activation='softmax', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = Dense(nb_classes, activation='softmax', kernel_initializer = glorot_uniform(seed=0))(X)
     
     # Create model
     model = Model(inputs = X_input, outputs = X)
@@ -220,7 +210,7 @@ def ResNet50(input_shape = (64, 64, 3), classes = 6, training=False):
 ##############################################################################################################
 
 # Create the model
-model = ResNet50(input_shape = (64, 64, 3), classes = 6)
+model = ResNet50(input_shape = (64, 64, 3), nb_classes = 6)
 
 # Get the summary 
 model.summary()
@@ -232,18 +222,18 @@ opt = tf.keras.optimizers.Adam(learning_rate=0.00015)
 model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Load the data
-X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_h5py_dataset(
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_h5_dataset(
     train_path='datasets/train_signs.h5',test_path='datasets/test_signs.h5')
 
-# Normalize the image vectors
+# Normalize the image vectors (for faster model convergence)
 X_train = X_train_orig / 255.
 X_test = X_test_orig / 255.
 
-# Convert training and test Y to one hot matrices
-Y_train = convert_to_one_hot(Y_train_orig, 6).T
-Y_test = convert_to_one_hot(Y_test_orig, 6).T
+# Convert training and test Y to one hot matrices (to use softmax later)
+Y_train = convert_to_one_hot(Y_train_orig, 6)
+Y_test = convert_to_one_hot(Y_test_orig, 6)
 
-# get an idea of the dimensions of train/test datasets
+# Get an idea of the dimensions of train/test datasets
 print ("Number of training examples = " + str(X_train.shape[0]))
 print ("Number of test examples = " + str(X_test.shape[0]))
 print ("X_train shape: " + str(X_train.shape))
@@ -251,7 +241,7 @@ print ("Y_train shape: " + str(Y_train.shape))
 print ("X_test shape: " + str(X_test.shape))
 print ("Y_test shape: " + str(Y_test.shape))
 
-# Train the model (with 20 epochs and only CPU, it takes around 1h)
+# Train the model
 model.fit(X_train, Y_train, epochs = 10, batch_size = 32)
 
 # Evaluate the model on test set
@@ -263,7 +253,7 @@ print ("Test Accuracy = " + str(preds[1])+" & Loss = " + str(preds[0]))
 #####                          USE A PRE-TRAINED MODEL AND TEST ON A NEW IMAGE                           #####
 ##############################################################################################################
 
-# This ResNet50 model's weights were trained on the SIGNS dataset
+# Load a pre-trained model (trained on the SIGNS dataset)
 pre_trained_model = load_model('resnet50.h5')
 
 # Get a summary of the pre-trained model
@@ -273,7 +263,7 @@ pre_trained_model.summary()
 preds = pre_trained_model.evaluate(X_test, Y_test)
 print ("Test Accuracy = " + str(preds[1]) + " & Loss = " + str(preds[0]))
 
-# Load an image, resize it, transform it to array
+# Load an image, resize it and transform it to array
 img = image.load_img('images/my_image.jpg', target_size=(64, 64))
 x = image.img_to_array(img)
 
@@ -290,5 +280,5 @@ plt.show()
 
 # Get the prediction of the image (using the pre-trained model)
 prediction = pre_trained_model.predict(x)
-print(np.argmax(prediction)) # we get the index with highest probability
-
+# Get the index with highest probability (so the predicted value)
+print(np.argmax(prediction)) 
